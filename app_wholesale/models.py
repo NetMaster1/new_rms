4cdd4a8e94ef2200b1076a3b9a5f9efc14dd4f69
NetMaster1 <BiZon-NN@yandex.ru>
2022-06-09 07:35:06 +0000

    remote

--- app_wholesale/models.py
+++ app_wholesale/models.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 
 # Create your models here.
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> 07ea3dc876783f7f59e43ee8188eb24b6f47dae0
--- app_wholesale/urls.py
+++ app_wholesale/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from . import views
 
@@ -7,14 +6,4 @@ urlpatterns = [
     path ('', views.wholesale_page, name='wholesale_page'),
     
 
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path ('', views.wholesale_page, name='wholesale_page'),
-    
-
->>>>>>> 07ea3dc876783f7f59e43ee8188eb24b6f47dae0
 ]
