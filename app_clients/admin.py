6d4ba450de1d2378f3410e95e7156523c52d9d09
Sergey Vinokurov <79200711112@yandex.ru>
2022-06-25 07:17:01 +0300

    24_06_22

--- app_clients/admin.py
+++ app_clients/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . models import Customer
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'f_name','l_name', 'created', 'phone', 'bar_code', 'accum_cashback', 'user')
+    list_display = ('id', 'l_name', 'created', 'phone', 'bar_code', 'accum_cashback', 'user')
 
 admin.site.register(Customer, CustomerAdmin)
