c0fb9ec5e8840f6dbc0ba5186ea4afded268226d
Sergey Vinokurov <79200711112@yandex.ru>
2023-10-31 12:53:44 +0300

    local

--- app_cash/admin.py
+++ app_cash/admin.py
@@ -4,8 +4,8 @@ from . models import Cash, Credit, Card, PaymentRegister #CashRemainder
 class CashAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'time_seconds', 'document', 'cho_type', 'shop', 'pre_remainder', 'cash_in', 'cash_out', 'current_remainder', 'cash_off_reason', 'cash_in_reason', 'cash_receiver')
     ordering = ('-created',)
-    list_filter = ('shop',)
-    list_per_page=25
+    list_filter = ('shop', 'cho_type', 'created',)
+    list_per_page=50
     list_select_related = True
 
     #I don't know how it works, but this functions created a separate column based on column 'created', but with more precise time '19 Feb 2022 15:54:00' instead of  'Feb. 21, 2022, 3:11 p.m.' I deleted 'created' from display_list. Somehow it may influence to filtering, but so far I have not noticed anything.
