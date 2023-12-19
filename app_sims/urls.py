8dd42664510305d0cfb82fa779a4e59714fcd5b5
unknown <NetMaster>
2023-12-19 13:02:54 +0300

    Added Reg Register Delete

--- app_sims/urls.py
+++ app_sims/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 urlpatterns = [
     path ('', views.sim_return_list, name='sim_return_list'),
-    path ('sim_register_list', views.sim_register_list, name='sim_register_list'),
+    
     path ('sim_return_report', views.sim_return_report, name='sim_return_report'),
     path ('change_sim_return_posted/<int:document_id>/', views.change_sim_return_posted, name='change_sim_return_posted'),
+    path ('delete_sim_return_posted/<int:document_id>/', views.delete_sim_return_posted, name='delete_sim_return_posted'),
+    #============================
+    path ('sim_register_list', views.sim_register_list, name='sim_register_list'),
     path ('change_sim_register_posted/<int:document_id>/', views.change_sim_register_posted, name='change_sim_register_posted'),
+    path ('delete_sim_register_posted/<int:document_id>/', views.delete_sim_register_posted, name='delete_sim_register_posted'),
+    #============================
     path ('activation_list', views.activation_list, name='activation_list'),
     path ('delete_sim_reports', views.delete_sim_reports, name='delete_sim_reports'),
     path ('sim_dispatch', views.sim_dispatch, name='sim_dispatch'),
