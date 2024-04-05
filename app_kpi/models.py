344b11dd04d68e87357e8be1707255ecba25ea54
NetMaster1 <bizon-nn@yandex.ru>
2024-04-05 18:48:31 +0300

    Edited Inventory page

--- app_kpi/models.py
+++ app_kpi/models.py
@@ -5,18 +5,42 @@ from app_product.models import Document
 from app_reference.models import DocumentType, Shop
 # Create your models here.
 
-class MonthlyPlanKPI(models.Model):
+class KPIMonthlyPlan(models.Model):
+    period_reported = models.CharField(max_length=50)#Отчетный месяц
     shop = models.ForeignKey(Shop, null=True, on_delete=models.DO_NOTHING)
     GI = models.IntegerField(default=0, null=True)
     MNP = models.IntegerField(default=0, null=True)
-    HI = models.IntegerField(default=0, null=True)
-    RT_equip_item = models.IntegerField(default=0, null=True)#number of pieces
+    HighBundle = models.IntegerField(default=0, null=True)#фокусные тарифы
+    HomeInternet = models.IntegerField(default=0, null=True)
+    smartphones_sum = models.IntegerField(default=0, null=True)
+    RT_active_cam = models.IntegerField(default=0, null=True)#number of pieces
     RT_equip_roubles = models.IntegerField(default=0, null=True)#sum of money
     wink_item = models.IntegerField(default=0, null=True)#number of pieces
     wink_roubles = models.IntegerField(default=0, null=True)#sum of money
     upsale= models.IntegerField(default=0, null=True)
     mixx= models.IntegerField(default=0, null=True)
-    gold_sim= models.IntegerField(default=0, null=True)
+    golden_sim= models.IntegerField(default=0, null=True)
+    insurance_charge= models.IntegerField(default=0, null=True)#оплата страховки
 
     def __int__(self):
         return self.id
+    
+    class KPI_performance(models.Model):
+        period_reported = models.CharField(max_length=50)#Отчетный месяц
+        shop = models.ForeignKey(Shop, null=True, on_delete=models.DO_NOTHING)
+        GI = models.IntegerField(default=0, null=True)
+        MNP = models.IntegerField(default=0, null=True)
+        HighBundle = models.IntegerField(default=0, null=True)#фокусные тарифы
+        HomeInternet = models.IntegerField(default=0, null=True)
+        smartphones_sum = models.IntegerField(default=0, null=True)
+        RT_active_cam = models.IntegerField(default=0, null=True)#number of pieces
+        RT_equip_roubles = models.IntegerField(default=0, null=True)#sum of money
+        wink_item = models.IntegerField(default=0, null=True)#number of pieces
+        wink_roubles = models.IntegerField(default=0, null=True)#sum of money
+        upsale= models.IntegerField(default=0, null=True)
+        mixx= models.IntegerField(default=0, null=True)
+        golden_sim= models.IntegerField(default=0, null=True)
+        insurance_charge= models.IntegerField(default=0, null=True)#оплата страховки
+
+        def __int__(self):
+            return self.id
