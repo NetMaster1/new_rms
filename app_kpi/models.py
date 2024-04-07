5cd4355a92d04bcf01c508e64359324d212047e7
NetMaster1 <bizon-nn@yandex.ru>
2024-04-08 02:56:49 +0300

    Edited KPI monthly plan

--- app_kpi/models.py
+++ app_kpi/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from app_product.models import Document
-from app_reference.models import DocumentType, Shop
+from app_reference.models import DocumentType, Shop, Month, Year
 # Create your models here.
 
 class KPIMonthlyPlan(models.Model):
-    period_reported = models.CharField(max_length=50)#Отчетный месяц
-    shop = models.ForeignKey(Shop, null=True, on_delete=models.DO_NOTHING)
+    shop = models.CharField(max_length=25, null=True)
+    month_reported = models.CharField(max_length=25, null=True)
+    year_reported = models.CharField(max_length=10, null=True)
+    #shop = models.ForeignKey(Shop, null=True, on_delete=models.DO_NOTHING)
+    # month_reported = models.ForeignKey(Month, null=True, on_delete=models.DO_NOTHING)#Отчетный месяц
+    # year_reported = models.ForeignKey(Year, null=True, on_delete=models.DO_NOTHING)#Отчетный месяц
     GI = models.IntegerField(default=0, null=True)
     MNP = models.IntegerField(default=0, null=True)
     HighBundle = models.IntegerField(default=0, null=True)#фокусные тарифы
@@ -25,22 +29,23 @@ class KPIMonthlyPlan(models.Model):
     def __int__(self):
         return self.id
     
-    class KPI_performance(models.Model):
-        period_reported = models.CharField(max_length=50)#Отчетный месяц
-        shop = models.ForeignKey(Shop, null=True, on_delete=models.DO_NOTHING)
-        GI = models.IntegerField(default=0, null=True)
-        MNP = models.IntegerField(default=0, null=True)
-        HighBundle = models.IntegerField(default=0, null=True)#фокусные тарифы
-        HomeInternet = models.IntegerField(default=0, null=True)
-        smartphones_sum = models.IntegerField(default=0, null=True)
-        RT_active_cam = models.IntegerField(default=0, null=True)#number of pieces
-        RT_equip_roubles = models.IntegerField(default=0, null=True)#sum of money
-        wink_item = models.IntegerField(default=0, null=True)#number of pieces
-        wink_roubles = models.IntegerField(default=0, null=True)#sum of money
-        upsale= models.IntegerField(default=0, null=True)
-        mixx= models.IntegerField(default=0, null=True)
-        golden_sim= models.IntegerField(default=0, null=True)
-        insurance_charge= models.IntegerField(default=0, null=True)#оплата страховки
+class KPI_performance(models.Model):
+    month_reported = models.ForeignKey(Month, null=True, on_delete=models.DO_NOTHING)#Отчетный месяц
+    year_reported = models.ForeignKey(Year, null=True, on_delete=models.DO_NOTHING)#Отчетный месяц
+    shop = models.ForeignKey(Shop, null=True, on_delete=models.DO_NOTHING)
+    GI = models.IntegerField(default=0, null=True)
+    MNP = models.IntegerField(default=0, null=True)
+    HighBundle = models.IntegerField(default=0, null=True)#фокусные тарифы
+    HomeInternet = models.IntegerField(default=0, null=True)
+    smartphones_sum = models.IntegerField(default=0, null=True)
+    RT_active_cam = models.IntegerField(default=0, null=True)#number of pieces
+    RT_equip_roubles = models.IntegerField(default=0, null=True)#sum of money
+    wink_item = models.IntegerField(default=0, null=True)#number of pieces
+    wink_roubles = models.IntegerField(default=0, null=True)#sum of money
+    upsale= models.IntegerField(default=0, null=True)
+    mixx= models.IntegerField(default=0, null=True)
+    golden_sim= models.IntegerField(default=0, null=True)
+    insurance_charge= models.IntegerField(default=0, null=True)#оплата страховки
 
-        def __int__(self):
-            return self.id
+    def __int__(self):
+        return self.id
--- app_kpi/views.py
+++ app_kpi/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from app_product.models import RemainderHistory, Document
-from app_reference.models import Shop, Product, DocumentType, ProductCategory
+from app_reference.models import Shop, Product, DocumentType, ProductCategory, Month, Year
 from app_reports.models import ReportTempId, Sim_report
+from .models import KPIMonthlyPlan, KPI_performance
 from django.contrib import messages
 import datetime
 from datetime import date, timedelta
 from django.contrib import messages, auth
+import pandas
 
-
-#creates a register of sim forms which has been returned to operator
 def monthly_kpi (request):
     if request.user.is_authenticated:
         date = datetime.date.today()
@@ -27,10 +27,140 @@ def monthly_kpi (request):
     
 
 def kpi_excel_input (request):
-    pass
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            file = request.FILES["file_name"]
+            df1 = pandas.read_excel(file)
+            cycle = len(df1)
+            for i in range(cycle):
+                row = df1.iloc[i]#reads each row of the df1 one by one
+
+                item=KPIMonthlyPlan.objects.create(
+                    shop=row.Shop,
+                    month_reported=row.Month,
+                    year_reported=row.Year,
+                    GI=row.GI,
+                    MNP=row.MNP,
+                    HighBundle=row.HighBundle,
+                    smartphones_sum=row.Smartphones,
+                    insurance_charge=row.Insurance,
+                    wink_roubles=row.Wink,
+                    HomeInternet=row.HI,
+                    RT_equip_roubles=row.RT_equip_roubles,
+                    RT_active_cam=row.RT_equip_items
+                )
+
+            return redirect ('log')
+        else:
+
+            return render(request, "kpi/kpi_auto_input.html")
+
+    else:
+        auth.logout(request)
+        return redirect("login")
 
 def kpi_adjustment (request):
     pass
 
-def kpi_execution (request):
-    pass
+
+
+def kpi_performance(request):
+    if request.user.is_authenticated:
+        shops=Shop.objects.all()
+        monthes=Month.objects.all()
+        years=Year.objects.all()
+
+        context = {
+            'shops': shops,
+            'monthes': monthes,
+            'years': years,
+        }
+        return render(request, "kpi/kpi_performance.html", context)
+    else:
+        auth.logout(request)
+        return redirect("login")
+    
+def kpi_monthly_report_per_shop (request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            # start_date = request.POST.get("start_date", False)
+            # if start_date:
+            #     start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+            # end_date = request.POST.get("end_date", False)
+            # if end_date:
+            #     end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+            #     end_date = end_date + timedelta(days=1)
+
+
+            shop = request.POST["shop"]
+            month = request.POST["month"]
+            month=Month.objects.get(id=month)
+            year = request.POST["year"]
+            year=Year.objects.get(id=year)
+            shop=Shop.objects.get(id=shop)
+            doc_type = DocumentType.objects.get(name="Продажа ТМЦ")
+            queryset = RemainderHistory.objects.filter(shop=shop, created__year=year.name, created__month=month.id, rho_type=doc_type)
+            category_sim=ProductCategory.objects.get(name='Сим_карты')
+            category_smartphones=ProductCategory.objects.get(name='Смартфоны')
+            category_insurance=ProductCategory.objects.get(name='Страховки')
+            category_wink=ProductCategory.objects.get(name='Подписки')
+            category_RT_equipment=ProductCategory.objects.get(name='Оборудование РТК')
+            #==========================================
+            query=queryset.filter(category=category_smartphones)
+            smartphones_sum=0
+            for item in query:
+                smartphones_sum+=item.retail_price
+            #==========================================
+            query=queryset.filter(category=category_sim)
+            number_of_sims=0
+            number_of_focus_sims=0
+            for item in query:
+                number_of_sims+=1
+                if item.retail_price>=650:
+                    number_of_focus_sims+=1
+            #==========================================
+            query=queryset.filter(category=category_RT_equipment)
+            RT_equipment_sum=0
+            for item in query:
+                RT_equipment_sum+=item.retail_price
+            #==========================================
+            query=queryset.filter(category=category_insurance)
+            insurance_sum=0
+            for item in query:
+                insurance_sum+=item.retail_price
+            #==========================================
+            query=queryset.filter(category=category_wink)
+            wink_sum=0
+            wink_items=0
+            for item in query:
+                wink_sum+=item.retail_price
+                wink_items+=1
+            
+            if KPI_performance.objects.filter(month_reported=month, year_reported=year).exists():
+                item=KPI_performance.objects.get(month_reported=month, year_reported=year)
+            else:
+                item=KPI_performance.objects.create(
+                    month_reported=month,
+                    year_reported=year,
+                    shop=shop,
+                    smartphones_sum=smartphones_sum,
+                    GI=number_of_sims,
+                    HighBundle=number_of_focus_sims,
+                    insurance_charge=insurance_sum,
+                    wink_roubles=wink_sum,
+                    wink_item=wink_items,
+                    RT_equip_roubles=RT_equipment_sum,
+                    )
+
+
+            context = {
+                'month': month,
+                'year': year,
+                'shop':shop,
+                'item': item,
+
+            }
+            return render(request, "kpi/kpi_per_shop.html", context)
+    else:
+        auth.logout(request)
+        return redirect("login")
