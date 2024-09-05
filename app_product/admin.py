8fdb28163ef85dd874e13e4e10022bcbf484403e
NetMaster1 <bizon-nn@yandex.ru>
2024-09-05 09:41:20 +0300

    local

--- app_product/admin.py
+++ app_product/admin.py
@@ -6,12 +6,12 @@ from . models import Document, RemainderHistory, Register, Identifier, AvPrice, 
 
 class RemainderHistoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'time_seconds', 'document', 'rho_type', 'status', 'shop', 'category', 'name', 'imei', 'pre_remainder', 'incoming_quantity', 'outgoing_quantity', 'current_remainder', 'wholesale_price', 'supplier', 'av_price', 'retail_price', 'user', 'inventory_doc', 'for_mp_sale', 'mp_RRP', ) 
-    list_filter = ('shop', 'rho_type', 'category', 'supplier')
+    list_filter = ('shop', 'rho_type', 'category', 'supplier',)
     ordering = ('-created',)
     list_per_page=50
     list_select_related = True
     list_editable = ('av_price', 'category', 'name', 'supplier')
-    search_fields = ('imei', 'document')
+    search_fields = ('imei',)
 
     #I don't know how it works, but this functions created a separate column based on column 'created', but with more precise time '19 Feb 2022 15:54:00' instead of  'Feb. 21, 2022, 3:11 p.m.' I deleted 'created' from display_list. Somehow it may influence to filtering, but so far I have not noticed anything.
     def time_seconds(self, obj):
