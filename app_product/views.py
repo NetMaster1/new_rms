d145a0589028255578bc14b29088105f9456ea35
NetMaster1 <BiZon-NN@yandex.ru>
2025-05-23 12:07:12 +0000

    remote

--- app_product/views.py
+++ app_product/views.py
@@ -772,6 +772,7 @@ def sale_input_cash(request, identifier_id, client_id, cashback_off):
             dateTime=request.POST.get('dateTime', False)
             if dateTime:
                 # converting dateTime in str format (2021-07-08T01:05) to django format ()
+                #function for converting time in django format to string format: 'strftime'
                 dateTime = datetime.datetime.strptime(dateTime, "%Y-%m-%dT%H:%M")
                 #adding seconds & microseconds to 'dateTime' since it comes as '2021-07-10 01:05:03:00' and we need it real value of seconds & microseconds
                 current_dt=datetime.datetime.now()
